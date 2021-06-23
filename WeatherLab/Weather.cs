4814a57527a2148a4c3598e821c6e6cb74bcc782
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-06-22 21:48:18 -0400

    Update Weather.cs

--- WeatherLab/Weather.cs
+++ WeatherLab/Weather.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
@@ -12,10 +12,15 @@ namespace WeatherLab
         public int year { get; set; }
         public int month { get; set; }
         public int day { get; set; }
-        public double? maxtemp { get; set; }
-        public double? mintemp { get; set; }
-        public double? meantemp { get; set; }
-        public double? precipitation { get; set; }
+        public string dateh { get; set; }
+        public string adate { get; set; }
+        public double hddv { get; set; }
+        public string datec { get; set; }
+        public double cvvv { get; set; }
+        public double maxtemp { get; set; }
+        public double mintemp { get; set; }
+        public double meantemp { get; set; }
+        public double precipitation { get; set; }
 
     }
 }
